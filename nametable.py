e5d8dbc560c1b067f53bb97765d614ba36f70b0e
Daniel Gullberg <daniel_gullberg@hotmail.com>
2017-07-18 23:36:38 +0200

    Print all bytes in tile map

--- nametable.py
+++ nametable.py
@@ -3,5 +3,29 @@ class NametableViewer:
         """Init"""
         pass
     
+class Tiles:
+    """Class for handling tiles"""
+    def __init__(self):
+        pass
+
+    def open(self, filename):
+        """Import file with tile data"""
+        with open(filename, "rb") as f:
+            byte = f.read(1)
+            col = 0
+            while byte !="":
+                if col>7:
+                    print
+                    col = 0
+                print "0x%02X" %(ord(byte)),
+                col += 1
+                byte = f.read(1)
+                
+                
+            
+        
+    
 if __name__ == "__main__":
-    nv = NametableViewer()
+    #nv = NametableViewer()
+    t = Tiles()
+    t.open(r"../game/src/test.chr")
